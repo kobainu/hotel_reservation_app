bcb3b779a0d8aa665412ac60b9d646868bd86c4b
小林大志 <kobayashihiroshi@kobayashihiroshinoMacBook-Air.local>
2022-09-15 23:57:23 +0900

    ルーティング作成(home, sign_up, sign_in, sign_out)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # ユーザーページにアクセス前に実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  # サインアップ時にnameを許可する
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
